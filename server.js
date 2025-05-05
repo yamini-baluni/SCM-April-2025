77f370a33a1743d881bde680c62896da851caf6f
DhruvKAggarwal123 <dhruv0130.becse24@chitkara.edu.in>
2025-05-05 21:19:28 +0530

    server.js update

--- server.js
+++ server.js
@@ -31,3 +31,5 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+//error handling for uncaught exceptions and unhandled rejections
